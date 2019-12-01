3965ec8bd53d467f750be670b55d44360a490de0
boniolo <33812322+rafaelboniolo@users.noreply.github.com>
2019-11-30 22:34:07 -0300

    Initial commit

---
+++ src/test/java/com/tcc/eureka/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
